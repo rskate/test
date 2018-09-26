30492a0b92ee6d3b2abef59e0a8b1365f86704a2
Alejandro <rskate@hotmail.com>
2018-09-25 20:02:12 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('============='.green);
+    console.log(`tabla del ${base}`.green);
+    console.log('============='.green);
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`)
+    }
+
+}
+
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introdució ${base}, no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (i = 1; i <= limite; i++) {
+            data += `${base} * ${ i } = ${ base * i}\n`;
+        }
+
+
+        fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
+            if (err)
+                reject(err);
+            else
+                resolve(`tabla-${ base }.txt`.green)
+        });
+
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
